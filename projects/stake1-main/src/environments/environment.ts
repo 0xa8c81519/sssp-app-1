a386d2e78637174aaf74711f7f0804e83df40757
张乐平 <zhangleping@192.168.1.8>
2021-04-01 14:47:51 +0800

    add stake1-main

---
+++ projects/stake1-main/src/environments/environment.ts
@@ -0,0 +1,25 @@
+export const environment = {
+    production: true,
+    poolId: "p1",
+    liquiditySymbol: "BSLP-01",
+    tokenSymbol: "BST",
+    virtualPriceDiff: 0.006,
+    coins: [{ symbol: 'bstDAI' }, { symbol: 'bstBUSD' }, { symbol: 'bstUSDT' }],
+    rpc: {
+        56: "'https://bsc-dataseed.binance.org/'",
+        97: 'https://data-seed-prebsc-1-s1.binance.org:8545/'
+    },
+    chains: {
+        56: {
+            enabled: false,
+            name: 'BSC Mainnet',
+            rpc: 'https://bsc-dataseed.binance.org/',
+            contracts: {
+                proxy: {
+                    address: "0x87Cbc493236DbB05132D5406dFCb039cF0E69bD7"
+                },
+                pid: 0
+            }
+        }
+    },
+};

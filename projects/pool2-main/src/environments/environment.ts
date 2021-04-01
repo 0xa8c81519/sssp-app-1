563d0bc753128bc924f7a3fcaf37f48bc1850b03
张乐平 <zhangleping@192.168.1.8>
2021-04-01 13:52:18 +0800

    add pool2-main

---
+++ projects/pool2-main/src/environments/environment.ts
@@ -0,0 +1,24 @@
+export const environment = {
+    production: false,
+    poolId: "p2",
+    liquiditySymbol: "BSLP-02",
+    tokenSymbol: "BST",
+    virtualPriceDiff: 0.006,
+    coins: [{ symbol: 'aBTC' }, { symbol: 'bBTC' }, { symbol: 'cBTC' }],
+    rpc: {
+        56: "'https://bsc-dataseed.binance.org/'",
+        97: 'https://data-seed-prebsc-1-s1.binance.org:8545/'
+    },
+    chains: {
+        56: {
+            enabled: true,
+            name: 'Testnet',
+            contracts: {
+                proxy: {
+                    address: "0xAcDe76E0175779088b8557a21b00F16D56651d25"
+                },
+                pid: 1
+            }
+        },
+    },
+};

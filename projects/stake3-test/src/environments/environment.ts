6631cf98db22e616a172b84bb07ff20989dc5a97
0xa8c81519 <0xa8c81519@gmail.com>
2021-04-24 17:17:54 +0800

    make like prod

---
+++ projects/stake3-test/src/environments/environment.ts
@@ -0,0 +1,24 @@
+export const environment = {
+    production: true,
+    poolId: "p2",
+    liquiditySymbol: "BSLP-03",
+    tokenSymbol: "BST",
+    virtualPriceDiff: 0.006,
+    coins: [{ symbol: 'tUSDC' }, { symbol: 'tBUSD' }, { symbol: 'tUSDT' }],
+    rpc: {
+        56: "'https://bsc-dataseed.binance.org/'",
+        97: 'https://data-seed-prebsc-1-s1.binance.org:8545/'
+    },
+    chains: {
+        97: {
+            enabled: true,
+            name: 'Testnet',
+            contracts: {
+                proxy: {
+                    address: "0xAcDe76E0175779088b8557a21b00F16D56651d25"
+                },
+                pid: 2
+            }
+        }
+    },
+};

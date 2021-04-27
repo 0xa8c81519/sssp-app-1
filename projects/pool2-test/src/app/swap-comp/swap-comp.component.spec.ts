e0a66a7fbf7edfe52db2d7f438547c91fcb8ba19
0xa8c81519 <0xa8c81519@gmail.com>
2021-04-27 15:12:49 +0800

    bug

--- projects/pool2-test/src/app/swap-comp/swap-comp.component.spec.ts
+++ projects/pool2-test/src/app/swap-comp/swap-comp.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SwapCompComponent } from './swap-comp.component copy';
+import { SwapCompComponent } from './swap-comp.component';
 
 describe('SwapCompComponent', () => {
   let component: SwapCompComponent;

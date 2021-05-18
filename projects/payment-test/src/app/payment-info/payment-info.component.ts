9f0d19c666964b887fb1fe08a90feca090ed90da
zhangpengfei <zhangpengfei>
2021-05-18 15:07:56 +0800

    Fix Bugs-0518

--- projects/payment-test/src/app/payment-info/payment-info.component.ts
+++ projects/payment-test/src/app/payment-info/payment-info.component.ts
@@ -1,9 +1,9 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
 import BigNumber from 'bignumber.js';
-import { CoinsDlgComponent } from '../coins-dlg/coins-dlg.component';
-import { BootService } from '../services/boot.service';
-import { ethers } from 'ethers';
+import {CoinsDlgComponent} from '../coins-dlg/coins-dlg.component';
+import {BootService} from '../services/boot.service';
+import {ethers} from 'ethers';
 
 export enum ApproveStatus {
     None, Approved, NoApproved
@@ -29,9 +29,9 @@ export class PaymentInfoComponent implements OnInit {
     left = 0;
     active = 1;
     slippageNumList: any = [
-        { num: 1 },
-        { num: 2 },
-        { num: 5 }
+        {num: 1},
+        {num: 2},
+        {num: 5}
     ];
 
     right = 1;
@@ -125,6 +125,7 @@ export class PaymentInfoComponent implements OnInit {
             });
         }
     }
+
     pay() {
         if (this.isOtherCurrency) {
             this.paySwap();
@@ -132,6 +133,7 @@ export class PaymentInfoComponent implements OnInit {
             this.payNoSwap();
         }
     }
+
     payNoSwap() {
         if (!this.address || this.address === '') {
             this.showDialog = false;
@@ -308,8 +310,13 @@ export class PaymentInfoComponent implements OnInit {
     }
 
     otherCurrency() {
-        this.isOtherCurrency = !this.isOtherCurrency;
-        this.calcNum();
+        if (Number(this.amt) > 0 && !this.isOtherCurrency) {
+            this.calcNum();
+            this.isOtherCurrency = !this.isOtherCurrency;
+        } else {
+            this.isOtherCurrency = false;
+            return;
+        }
         this.updateApproveStatusRight();
         if (!this.isOtherCurrency) {
             this.insufficientLiquidity = false;
@@ -324,7 +331,7 @@ export class PaymentInfoComponent implements OnInit {
     calcNum() {
         if (this.isOtherCurrency) {
             this.boot.estimatePayWithSwapGasFee().then(_gasFee => {
-                this.gasFee = (_gasFee.quote === 'USD' ? '$' : '') + _gasFee.data.toFormat(2, BigNumber.ROUND_DOWN) + " " + _gasFee.quote;
+                this.gasFee = (_gasFee.quote === 'USD' ? '$' : '') + _gasFee.data.toFormat(2, BigNumber.ROUND_DOWN) + ' ' + _gasFee.quote;
             });
             this.receiptAmt = '-';
             this.boot.getExchangeOutAmt(this.right, this.left, this.rightAmt).then(amt => {
@@ -337,7 +344,7 @@ export class PaymentInfoComponent implements OnInit {
         } else {
             this.receiptAmt = new BigNumber(this.amt).multipliedBy(1 - .003).toFormat(2, BigNumber.ROUND_DOWN);
             this.boot.estimatePayGasFee().then(_gasFee => {
-                this.gasFee = (_gasFee.quote === 'USD' ? '$' : '') + _gasFee.data.toFormat(2, BigNumber.ROUND_DOWN) + " " + _gasFee.quote;
+                this.gasFee = (_gasFee.quote === 'USD' ? '$' : '') + _gasFee.data.toFormat(2, BigNumber.ROUND_DOWN) + ' ' + _gasFee.quote;
             }).catch(e => {
                 console.log(e);
             });
